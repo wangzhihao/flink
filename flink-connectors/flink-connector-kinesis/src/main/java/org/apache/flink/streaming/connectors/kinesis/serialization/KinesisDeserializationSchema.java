a9d9f5b34283b277806ef416e02b97e28d8ef659
Zhihao Wang <accept.acm@gmail.com>
2021-01-12 00:14:58 +0000

    add 1 to n emitter interface

--- flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/serialization/KinesisDeserializationSchema.java
+++ flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/serialization/KinesisDeserializationSchema.java
@@ -71,6 +71,41 @@ public interface KinesisDeserializationSchema<T> extends Serializable, ResultTyp
             throws IOException;
 
     /**
+     * Deserializes the Kinesis record.
+     *
+     * <p>Can output multiple records through the {@link Collector}. Note that number and size of
+     * the produced records should be relatively small. Depending on the source implementation
+     * records can be buffered in memory or collecting records might delay emitting checkpoint
+     * barrier.
+     *
+     *
+     * @param recordValue the record's value as a byte array
+     * @param partitionKey the record's partition key at the time of writing
+     * @param seqNum the sequence number of this record in the Kinesis shard
+     * @param approxArrivalTimestamp the server-side timestamp of when Kinesis received and stored
+     *     the record
+     * @param stream the name of the Kinesis stream that this record was sent to
+     * @param shardId The identifier of the shard the record was sent to
+     * @param out The collector to put the resulting messages.
+     * @throws IOException
+     */
+    default void deserialize(
+            byte[] recordValue,
+            String partitionKey,
+            String seqNum,
+            long approxArrivalTimestamp,
+            String stream,
+            String shardId,
+            Collector<T> out)
+            throws IOException;
+        T deserialized = deserialize(recordValue, partitionKey, seqNum,
+                approxArrivalTimestamp, stream, shardId);
+        if (deserialized != null) {
+            out.collect(deserialized);
+        }
+    }
+
+    /**
      * Method to decide whether the element signals the end of the stream. If true is returned the
      * element won't be emitted.
      *
